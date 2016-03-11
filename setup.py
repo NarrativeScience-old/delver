d3b0cdbac056ab48966a39e19477937a1849b0de
Alex Sippel <asippel@narrativescience.com>
2016-03-11 09:55:46 -0600

    Fixed setup.py so script installs correctly

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 import setuptools
 import os
 
-# This will add the __version__ to the globals
-execfile('src/__init__.py')
-
 setuptools.setup(
-    name='data_explorer',
-    version=__version__,
+    name='explorer',
+    version='0.0.1',
     package_dir={'': 'src'},
     packages=setuptools.find_packages('src'),
     provides=setuptools.find_packages('src'),
     entry_points={
-        'console_scripts': ['explore = explore:main']
+        'console_scripts': [
+            'explore = explorer.explore:main'
+        ]
     }
 )
